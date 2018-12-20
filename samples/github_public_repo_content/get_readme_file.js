a3e277f0509dafdbbcfbde1e381c023146aa7201
mickelmansour <mhm24@njit.edu>
2018-12-20 16:43:14 -0500

    fix github url

--- samples/github_public_repo_content/get_readme_file.js
+++ samples/github_public_repo_content/get_readme_file.js
@@ -1,14 +1,14 @@
 const request = require('request');
 
 // Send GET request to Github API with the following url: 
-//     https://api.github.com/repos/<owner>/<repo name>/<path to file or folder (case sensitive)>
+//     https://api.github.com/repos/<username>/<repo name>/contents/<path to file or folder (case sensitive)>
 request.get({
-        url: 'https://api.github.com/repos/mickelmansour/personal-site/README.md',
+        url: 'https://api.github.com/repos/mickelmansour/personal-site/contents/README.md',
         headers: {
             'User-Agent': 'node app'
         }
     }, (error, response, body) => {
-	// The file information is returned in body, with encoded content
+	// The README.md file information is returned in body, with encoded content
 	// NOTE: If the path provided in the url maps to a folder, details about 
 	//       each child file/folder will be returned. 
         console.log(JSON.parse(body));
